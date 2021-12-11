92fcd6e65d0c4ba7e97b54fb80c44cd6e1714a71
Jean-Philippe Bidegain <jp@bidega.in>
2021-12-11 11:24:11 +0100

    feat: display the received date in a relative format

--- assets/script/components/message-list-item/MessageListItem.vue
+++ assets/script/components/message-list-item/MessageListItem.vue
@@ -3,7 +3,7 @@
     <td>{{ from }}</td>
     <td>{{ to }}</td>
     <td>{{ message.subject }}</td>
-    <td>{{ message.created_at }}</td>
+    <td><abbr :title="formattedDate">{{ relativeDate }}</abbr></td>
     <td><router-link :to="{ name: 'message', params: { id: message.id }}" class="btn btn-white">View</router-link></td>
   </tr>
 </template>
@@ -11,10 +11,14 @@
 <script>
 import { parseEmail } from '../../helpers/email';
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime)
+
 export default {
   name: 'message-list-item',
   props: {
-    message: Object
+    message: Object,
   },
   computed: {
     from: function () {
@@ -22,7 +26,13 @@ export default {
     },
     to: function() {
       return this.message.recipients.map(recipient => parseEmail(recipient)).join(', ');
-    }
+    },
+    formattedDate: function () {
+      return dayjs(this.message.created_at).format('DD MMMM YYYY, hh:mm:ss');
+    },
+    relativeDate: function () {
+      return dayjs().to(dayjs(this.message.created_at));
+    },
   }
 };
 </script>
