4a2333562677c45bc45a25d3db3c389e30312b37
Jean-Philippe Bidegain <jp@bidega.in>
2021-12-11 15:43:08 +0100

    feat: display the source of an email

--- assets/index.js
+++ assets/index.js
@@ -3,4 +3,5 @@ import './style/app.scss';
 
 // Script
 import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/tab';
 import './script/app.js';
--- assets/script/pages/MessagePage.vue
+++ assets/script/pages/MessagePage.vue
@@ -1,25 +1,45 @@
 <template>
-  <div class="message card">
-    <div class="card-body">
-      <div class="row">
-        <div class="col-sm-6">
-          <dl class="row">
-            <dt class="col-sm-2">From</dt>
-            <dd class="col-sm-9">
-              <span v-if="message && from">{{ from }}</span>
-              <div v-else class="skeleton-line skeleton-line-full"></div>
-            </dd>
-            <dt class="col-sm-2">Subject</dt>
-            <dd class="col-sm-9">
-              <span v-if="message && message.subject">{{ message.subject }}</span>
-              <div v-else class="skeleton-line skeleton-line-full"></div>
-            </dd>
-            <dt class="col-sm-2">To</dt>
-            <dd class="col-sm-9">
-              <span v-if="message && to">{{ to }}</span>
-              <div v-else class="skeleton-line skeleton-line-full"></div>
-            </dd>
-          </dl>
+  <div class="message">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="card">
+          <div class="card-body">
+            <dl class="row">
+              <dt class="col-sm-4">From</dt>
+              <dd class="col-sm-8">
+                <span v-if="message && from">{{ from }}</span>
+                <div v-else class="skeleton-line skeleton-line-full"></div>
+              </dd>
+              <dt class="col-sm-4">Subject</dt>
+              <dd class="col-sm-8">
+                <span v-if="message && message.subject">{{ message.subject }}</span>
+                <div v-else class="skeleton-line skeleton-line-full"></div>
+              </dd>
+              <dt class="col-sm-4">To</dt>
+              <dd class="col-sm-8">
+                <span v-if="message && to">{{ to }}</span>
+                <div v-else class="skeleton-line skeleton-line-full"></div>
+              </dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-9">
+        <div class="card" v-if="message">
+          <ul class="nav nav-tabs" data-bs-toggle="tabs">
+            <li class="nav-item" v-for="format in message.formats" :key="format">
+              <a :href="'#tabs-' + format" class="nav-link active" data-bs-toggle="tab">{{ format }}</a>
+            </li>
+          </ul>
+
+          <div class="card-body">
+            <div class="tab-content">
+              <div class="tab-pane active" :id="'tabs-' + format"  v-for="(messageByFormat, format) in messageByFormats" :key="format">
+                <pre><code>{{ messageByFormat }}</code></pre>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -33,19 +53,43 @@ export default {
   name: 'message-page',
   data: function () {
     return {
-      message: null
+      message: null,
+      messageByFormats: {},
     };
+  },
+  methods: {
+    getMessage(messageId) {
+      return this
+        .axios
+        .get(`http://127.0.0.1:1080/messages/${messageId}`);
+    },
+    getFormat(messageId, format) {
+      return this
+        .axios
+        .get(`http://127.0.0.1:1080/messages/${messageId}/${format}`);
+    },
+    async init() {
+      const messageId = this.$route.params.id;
+
+      await this
+        .getMessage(messageId)
+        .then(response => {
+          const message = response.data;
+          this.message = message;
+        });
+
+      await this.message.formats.forEach(format => {
+        this
+          .getFormat(messageId, format)
+          .then(response => this.$set(this.messageByFormats, format, response.data));
+      });
+
+
+    }
   },
   mixins: [emailMixin],
   mounted: function () {
-    const messageId = this.$route.params.id;
-    this
-      .axios
-      .get(`http://127.0.0.1:1080/messages/${messageId}`)
-      .then((response) => {
-        const message = response.data;
-        this.message = message;
-      });
+    this.init();
   }
 };
 </script>
